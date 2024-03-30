d2a736d87ac6003d2d3f0ef58e990fe3616c0411
AyubxonUz <yuldoshevv7@gmail.com>
2024-03-30 22:17:20 +0500

    ProjectList updated design

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -11,12 +11,12 @@ function ProjectsList() {
             className="object-cover rounded-md"
             alt="Project image"
           />
-          <h4 className="font-semibold text-xl mt-4">{prod.title}</h4>
-          <p className="font-medium mt-2">
-            Info: <span className="font-light">{prod.info}</span>
+          <h4 className="font-bold text-xl mt-4">{prod.title}</h4>
+          <p className="font-bold mt-2">
+            Info: <span className="font-normal">{prod.info}</span>
           </p>
-          <p className="font-medium mt-2">
-            Technology: <span className="font-light">{prod.technology}</span>
+          <p className="font-bold mt-2">
+            Technology: <span className="font-normal">{prod.technology}</span>
           </p>
           <div className="flex gap-x-3 mt-6">
             <ButtonLink link={prod.githubLink} text="Github" />
